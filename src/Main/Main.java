c17b04d63a8328cd2ad3b0823beaec361b66173b
Vadym Kharchenko <glassofspace@gmail.com>
2021-10-31 12:53:58 -0700

    Finished - CP4

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -2,15 +2,18 @@ package Main;
 
 import java.awt.Color;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Stack;
+import java.util.StringTokenizer;
 
 import Data.Vector2D;
 import Data.spriteInfo;
+import FileIO.EZFileRead;
 import logic.Control;
 import timer.stopWatchX;
 
-//int vindexN3 = isSupported(1280, 720, 32, dms);
 public class Main
 {
 	// Fields (Static) below...
@@ -24,48 +27,17 @@ public class Main
 	public static Vector2D currentVec = new Vector2D(-100,-100);
 	public static int step = 8;
 	public static int i = 1;
-	//CP3
+//	CP3
 	public static ArrayList<spriteInfo> sprites = new ArrayList<>();
 	public static int currentSpriteIndex = 0;
+//	CP4
+	public static HashMap<String, String> map = new HashMap<>();
 	// End Static fields...
 
 	public static void main(String[] args)
 	{
 		Control ctrl = new Control();				// Do NOT remove!
 		ctrl.gameLoop();						    // Do NOT remove
-
-		 //TESTINGS
-//		Vector2D vecTemp = new Vector2D(0, 250);
-//		int i = 0;
-//		spriteInfo newObj;
-//		int x = -100;
-//
-//		while (vecTemp.getX() <= 1280)
-//		{
-//			newObj = new spriteInfo(vecTemp, "fr"+i);
-//			sprites.add(newObj);
-//			vecTemp = new Vector2D(x, 320);
-//			x+=step;
-//			i+=1;
-//			if(i==9) i = 1;
-//		}
-//		//vecs1.remove;
-//
-//
-//		for (currentSpriteIndex = 0; currentSpriteIndex < sprites.size(); currentSpriteIndex++)
-//		{
-//			spriteInfo tmp = sprites.get(currentSpriteIndex);
-//			currentVec = tmp.getCoords();
-//			//ctrl.addSpriteToFrontBuffer(currentVec.getX(), currentVec.getY(),tmp.getTag());
-//			
-//			System.out.printf("  TEST"+currentSpriteIndex+": " + currentVec.getX() + "  " + currentVec.getY() + "  " + tmp.getTag());
-//		}
-//		
-//		//spriteInfo tmp = sprites.get(32);
-//		 //tmp = sprites.get(47);
-//		//System.out.printf("  TEST"+currentSpriteIndex+": " + currentVec.getX() + "  " + currentVec.getY() + "  " + tmp.getTag());
-//		//System.out.printf("  TEST1: " + newObj.toString()); //currentVec.getX()
-//		//System.out.printf("  TEST2: " + vecTemp.getX() + "   " + vecTemp.getY());
 	}
 
 	/* This is your access to things BEFORE the game loop starts */
@@ -74,44 +46,25 @@ public class Main
 
 		// TODO: Code your starting conditions here...NOT DRAW CALLS HERE! (no addSprite or drawString)
 
-//CP2
-		Vector2D vecTemp = new Vector2D(0, 250);
-		int i = 1;
-		int x = -100;
-		spriteInfo newObj; //placeholder
-		while (vecTemp.getX() <= 1280)
+		EZFileRead ezr = new EZFileRead("cp4.txt");
+		StringTokenizer st;
+		String Key, Value, str;
+		for (int i = 0; i < ezr.getNumLines(); i++)
 		{
-			newObj = new spriteInfo(vecTemp, "fr"+i);
-			sprites.add(newObj);
-			vecTemp = new Vector2D(x, 320);
-			x+=step;
-			i+=1;
-			if(i==9) i = 1;
+			str = ezr.getLine(i);
+			st = new StringTokenizer(str, "*");
+			Key = st.nextToken();
+			Value = st.nextToken();
+			map.put(Key, Value);
 		}
-		sprites.remove(0);
-
 	}
 
 	/* This is your access to the "game loop" (It is a "callback" method from the Control class (do NOT modify that class!))*/
 	public static void update(Control ctrl)
 		{
 		// TODO: This is where you can code! (Starting code below is just to show you how it works)
-
-		//ctrl.drawString(0, 700, "x: "+ vecs1.toString(), light_green);
-//CP2	
-		spriteInfo tmp;
-			
-		tmp = sprites.get(currentSpriteIndex);
-		currentVec =  tmp.getCoords();
-		ctrl.addSpriteToFrontBuffer(currentVec.getX(), currentVec.getY(),tmp.getTag()); //currentVec.getX(), currentVec.getY(),tmp.getTag()
-		
-			if(timer.isTimeUp())
-			{
-				currentSpriteIndex++;
-				if (currentSpriteIndex == sprites.size()) currentSpriteIndex = 0;
-				timer.resetWatch();
-			}
-
+		String OrdStickmanString1 = map.get("string1"); // Pass the first Key
+		ctrl.drawString(100, 250, OrdStickmanString1, light_green);
 		}
 
 	// Additional Static methods below...(if needed)
